eeaf861cf7ebea5931ed80c266bb96a260224e36
mysd33 <mysd.33@gmail.com>
2024-11-23 18:41:46 +0900

    Repositoryの性能ログ、非同期実行依頼ログを出力するための対応

--- src/main/java/com/example/fw/common/async/repository/JobRequestRepositoryImpl.java
+++ src/main/java/com/example/fw/common/async/repository/JobRequestRepositoryImpl.java
@@ -4,8 +4,12 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.util.Assert;
 
 import com.example.fw.common.async.model.JobRequest;
+import com.example.fw.common.logging.ApplicationLogger;
+import com.example.fw.common.logging.LoggerFactory;
+import com.example.fw.common.message.CommonFrameworkMessageIds;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 
@@ -13,15 +17,18 @@ import lombok.RequiredArgsConstructor;
  *
  */
 @RequiredArgsConstructor
+@Slf4j
 public class JobRequestRepositoryImpl implements JobRequestRepository {
+	private static final ApplicationLogger appLogger = LoggerFactory.getApplicationLogger(log);
     private final JmsTemplate jmsTemplate;
     private final String queueName;
 
     @Override
     public void save(final JobRequest jobRequest) {
-        Assert.notNull(jobRequest, "jobRequestがNullです");
+        Assert.notNull(jobRequest, "jobRequestがNullです");        
         // キューに登録
         jmsTemplate.convertAndSend(queueName, jobRequest);
+        appLogger.info(CommonFrameworkMessageIds.I_CM_FW_0005, queueName, jobRequest);
     }
 
 }
