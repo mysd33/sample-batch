258fb7db4b304e4944edf63bd1b77e2f01e7f1ad
mysd33 <mysd.33@gmail.com>
2025-03-27 23:07:34 +0900

    お作法として、スレッドローカルの中身を消す対応

--- src/main/java/com/example/fw/batch/store/DefaultJmsMessageManager.java
+++ src/main/java/com/example/fw/batch/store/DefaultJmsMessageManager.java
@@ -38,6 +38,8 @@ public class DefaultJmsMessageManager implements JmsMessageManager {
                 // メッセージを削除できなくても例外はスローしない。
                 // メッセージ再受信する可能性があるが、AsyncMessageListenerでハンドリングし対処
                 appLogger.warn(BatchFrameworkMessageIds.W_BT_FW_8006, messageId);
+            } finally {
+                jmsMessageStore.remove();
             }
         }
     }
