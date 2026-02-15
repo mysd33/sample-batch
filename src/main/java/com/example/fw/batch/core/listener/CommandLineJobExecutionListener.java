cef2b2a9a95596318e902199c098891b665435a8
mysd33 <mysd.33@gmail.com>
2026-02-15 14:54:28 +0900

    コマンドライン実行機能のリファクタ

--- src/main/java/com/example/fw/batch/core/listener/CommandLineJobExecutionListener.java
+++ src/main/java/com/example/fw/batch/core/listener/CommandLineJobExecutionListener.java
@@ -5,8 +5,6 @@ import java.time.LocalDateTime;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobExecutionListener;
 
-import com.example.fw.batch.async.config.SQSServerConfigurationProperties;
-import com.example.fw.batch.async.store.JmsMessageManager;
 import com.example.fw.batch.core.exception.ExceptionHandler;
 import com.example.fw.batch.message.BatchFrameworkMessageIds;
 import com.example.fw.common.logging.ApplicationLogger;
@@ -16,31 +14,19 @@ import com.example.fw.common.systemdate.SystemDateUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-//TODO: ディレードの場合のコードが混在しているので、
-//ディレード用のDefaultJboExecutionListenerとクラスを分ける
-
 /**
  * キューからメッセージ削除、例外ハンドリング、ログ出力を行うJobExecutionListener
  */
 @Slf4j
 @RequiredArgsConstructor
-public class DefaultJobExecutionListener implements JobExecutionListener {
+public class CommandLineJobExecutionListener implements JobExecutionListener {
     private static final ApplicationLogger appLogger = LoggerFactory.getApplicationLogger(log);
     private final ExceptionHandler defaultExceptionHandler;
-    private final JmsMessageManager jmsMessageManager;
-    private final SQSServerConfigurationProperties sqsServerConfigurationProperties;
 
     @Override
     public void beforeJob(JobExecution jobExecution) {
         appLogger.info(BatchFrameworkMessageIds.I_FW_BTCTRL_0001, jobExecution.getJobInstance().getJobName(),
                 jobExecution.getJobId(), jobExecution.getId());
-        // isAckOnJobStartがtrueの場合、ジョブ開始後即時にキューメッセージをACK（削除）する
-        // 長時間バッチではSQSの可視性タイムアウトを短くするためtrueにするとよい
-        if (jmsMessageManager != null && //
-                sqsServerConfigurationProperties != null && sqsServerConfigurationProperties.isAckOnJobStart()) {
-            // ACKしキューからメッセージを削除
-            jmsMessageManager.acknowledge();
-        }
     }
 
     @Override
